51ac3cfc64ee0b15be05d72f6d532b4f2242d621
Cedryk <cedryk.doucet@polymtl.ca>
2018-05-31 14:25:13 -0400

    Added gui for parameter tab

---
+++ ParameterWidget.py
@@ -0,0 +1,44 @@
+from PyQt5 import QtWidgets
+import Parameter_Widget
+class valueType():
+    MARGIN = 0
+    VALUE = 1
+
+class ParameterWidget():
+    def __init__(self, param, sample):
+        self.widget  = QtWidgets.QWidget()
+        self.paramWidget = Parameter_Widget.Ui_ParameterWidget()
+        self.paramWidget.setupUi(self.widget)
+        self.paramWidget.paramLabel.setText(param)
+        self.param = param
+        self.sample = sample
+        self.paramWidget.marginListWidget.currentTextChanged.connect(lambda text: self.pairSelected(text, valueType.MARGIN))
+        self.paramWidget.worstListWidget.currentTextChanged.connect(lambda text: self.pairSelected(text, valueType.VALUE))
+        self.setPairsList()
+        self.paramWidget.passLabel.setText("TODO")
+
+    def setPairsList(self):
+        self.paramWidget.marginListWidget.addItems(self.sample.port_name.values())
+        self.paramWidget.marginListWidget.sortItems()
+        self.paramWidget.marginListWidget.setCurrentRow(0)
+        self.paramWidget.worstListWidget.addItems(self.sample.port_name.values())
+        self.paramWidget.worstListWidget.sortItems()
+        self.paramWidget.worstListWidget.setCurrentRow(0)
+
+    def pairSelected(self, pair, listIndex):
+        #TODO: get value of pair
+        self.setLabels(listIndex, pair)
+
+    def setLabels(self, listIndex, pair):
+        if listIndex == valueType.MARGIN: #Worst margin
+            self.paramWidget.marginValueLabel.setText(pair)
+            self.paramWidget.marginFreqLabel.setText('')
+            self.paramWidget.marginLimitLabel.setText('')
+            self.paramWidget.marginLabel.setText('')
+        else: #worst value
+            self.paramWidget.worstValueLabel.setText(pair)
+            self.paramWidget.worstFreqLabel.setText('')
+            self.paramWidget.worstLimitLabel.setText('')
+            self.paramWidget.worstMarginLabel.setText('')
+
+
