2acdde679174ccc77264b68dcb96f44820834bc0
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-06-13 09:48:04 -0400

    Update NEXT parameter to be a PairedParameter

--- parameters/next.py
+++ parameters/next.py
@@ -1,25 +1,25 @@
-from parameters.parameter import Parameter, complex2db
+from parameters.parameter import PairedParameter, complex2db, order
 
-class NEXTSingleEnded(Parameter):
-    def computeParameter(self):
-        # Example of NEXT for 6 single ended wires.
-        #
-        #        1 2 3 4 5 6
-        #
-        #   1   [_ 1 2 _ _ _]
-        #   2   [1 _ 3 _ _ _]
-        #   3   [2 3 _ _ _ _]
-        #   4   [_ _ _ _ 4 5]
-        #   5   [_ _ _ 4 _ 6]
-        #   6   [_ _ _ 5 6 _]
-        #
+class NEXTSingleEnded(PairedParameter):
+    """
+    Example of NEXT for 6 single ended wires.
+    
+            1 2 3 4 5 6
+    
+       1   [_ 1 2 _ _ _]
+       2   [1 _ 3 _ _ _]
+       3   [2 3 _ _ _ _]
+       4   [_ _ _ _ 4 5]
+       5   [_ _ _ 4 _ 6]
+       6   [_ _ _ 5 6 _]
+
+    We have the following pairs: (1,2), (1,3), (2,3), (4,5), (4,6), (5,6).
+    """
+    def computePairs(self):
         numPorts = len(self._ports)
 
-        # initialize the dictionaries for each port
-        (dbNEXT, cpNEXT) = (dict(), dict())
-
         # create each pair for the NEXT
-        pairs = list()
+        pairs = set()
         for i in range(0, numPorts//2):
             for j in range(0, numPorts//2):
                 if i >= j:
@@ -28,28 +28,31 @@ class NEXTSingleEnded(Parameter):
                 # create the pair for the first end of the line
                 port1 = i
                 port2 = j
-                pairname = self._ports[port1] + "-" + self._ports[port2]
-                pairs.append((port1, port2, pairname))
-                dbNEXT[pairname] = list()
-                cpNEXT[pairname] = list()
+                pairs.add((port1, port2))
 
                 # create the pair for the second end of the line
                 port1 = i + numPorts//2
                 port2 = j + numPorts//2
-                pairname = self._ports[port1] + "-" + self._ports[port2]
-                pairs.append((port1, port2, pairname))
-                dbNEXT[pairname] = list()
-                cpNEXT[pairname] = list()
+                pairs.add(order(port1, port2))
+
+        return pairs
+
+    def computeParameter(self):
+        # initialize the dictionaries for each port
+        (dbNEXT, cpNEXT) = (dict(), dict())
+        for (i,j) in self._pairs:
+            dbNEXT[(i,j)] = list()
+            cpNEXT[(i,j)] = list()
 
         # extract the NEXT values from the matrices
         for (f,_) in enumerate(self._freq):
-            for (i,j,pair) in pairs:
+            for (i,j) in self._pairs:
                 # get the value from the matrix
                 cpValue = self._matrices[f, i, j]
                 dbValue = complex2db(cpValue)
 
                 # add the value into the NEXT
-                cpNEXT[pair].append(cpValue)
-                dbNEXT[pair].append(dbValue)
+                cpNEXT[(i,j)].append(cpValue)
+                dbNEXT[(i,j)].append(dbValue)
 
         return (dbNEXT, cpNEXT)
--- parameters/test_next.py
+++ parameters/test_next.py
@@ -13,24 +13,24 @@ class TestNEXTSingleEnded(TestParameter):
         self.assertEqual(len(parameter), 2)
 
         # make sure the pair were created
-        self.assertEqual("Port 1-Port 2" in parameter, True)
-        self.assertEqual("Port 3-Port 4" in parameter, True)
+        self.assertEqual((0,1) in parameter, True)
+        self.assertEqual((2,3) in parameter, True)
 
         # the number of sample should be the same as the number of frequencies
-        self.assertEqual(len(parameter["Port 1-Port 2"]), len(self._freq))
-        self.assertEqual(len(parameter["Port 3-Port 4"]), len(self._freq))
+        self.assertEqual(len(parameter[(0,1)]), len(self._freq))
+        self.assertEqual(len(parameter[(2,3)]), len(self._freq))
 
         # check the values of the pair 1
-        self.assertAlmostEqual(parameter["Port 1-Port 2"][0], complex2db(self._matrices[0, 0, 1]))
-        self.assertAlmostEqual(parameter["Port 1-Port 2"][1], complex2db(self._matrices[1, 0, 1]))
-        self.assertAlmostEqual(parameter["Port 1-Port 2"][2], complex2db(self._matrices[2, 0, 1]))
-        self.assertAlmostEqual(parameter["Port 1-Port 2"][3], complex2db(self._matrices[3, 0, 1]))
+        self.assertAlmostEqual(parameter[(0,1)][0], complex2db(self._matrices[0, 0, 1]))
+        self.assertAlmostEqual(parameter[(0,1)][1], complex2db(self._matrices[1, 0, 1]))
+        self.assertAlmostEqual(parameter[(0,1)][2], complex2db(self._matrices[2, 0, 1]))
+        self.assertAlmostEqual(parameter[(0,1)][3], complex2db(self._matrices[3, 0, 1]))
 
         # check the values of the pair 2
-        self.assertAlmostEqual(parameter["Port 3-Port 4"][0], complex2db(self._matrices[0, 2, 3]))
-        self.assertAlmostEqual(parameter["Port 3-Port 4"][1], complex2db(self._matrices[1, 2, 3]))
-        self.assertAlmostEqual(parameter["Port 3-Port 4"][2], complex2db(self._matrices[2, 2, 3]))
-        self.assertAlmostEqual(parameter["Port 3-Port 4"][3], complex2db(self._matrices[3, 2, 3]))
+        self.assertAlmostEqual(parameter[(2,3)][0], complex2db(self._matrices[0, 2, 3]))
+        self.assertAlmostEqual(parameter[(2,3)][1], complex2db(self._matrices[1, 2, 3]))
+        self.assertAlmostEqual(parameter[(2,3)][2], complex2db(self._matrices[2, 2, 3]))
+        self.assertAlmostEqual(parameter[(2,3)][3], complex2db(self._matrices[3, 2, 3]))
 
     def testComputeComplexParameter(self):
         NEXT = NEXTSingleEnded(self._ports, self._freq, self._matrices)
@@ -40,24 +40,24 @@ class TestNEXTSingleEnded(TestParameter):
         self.assertEqual(len(parameter), 2)
 
         # make sure the pair were created
-        self.assertEqual("Port 1-Port 2" in parameter, True)
-        self.assertEqual("Port 3-Port 4" in parameter, True)
+        self.assertEqual((0,1) in parameter, True)
+        self.assertEqual((2,3) in parameter, True)
 
         # the number of sample should be the same as the number of frequencies
-        self.assertEqual(len(parameter["Port 1-Port 2"]), len(self._freq))
-        self.assertEqual(len(parameter["Port 3-Port 4"]), len(self._freq))
+        self.assertEqual(len(parameter[(0,1)]), len(self._freq))
+        self.assertEqual(len(parameter[(2,3)]), len(self._freq))
 
         # check the values of the pair 1
-        self.assertAlmostEqual(parameter["Port 1-Port 2"][0], self._matrices[0, 0, 1])
-        self.assertAlmostEqual(parameter["Port 1-Port 2"][1], self._matrices[1, 0, 1])
-        self.assertAlmostEqual(parameter["Port 1-Port 2"][2], self._matrices[2, 0, 1])
-        self.assertAlmostEqual(parameter["Port 1-Port 2"][3], self._matrices[3, 0, 1])
+        self.assertAlmostEqual(parameter[(0,1)][0], self._matrices[0, 0, 1])
+        self.assertAlmostEqual(parameter[(0,1)][1], self._matrices[1, 0, 1])
+        self.assertAlmostEqual(parameter[(0,1)][2], self._matrices[2, 0, 1])
+        self.assertAlmostEqual(parameter[(0,1)][3], self._matrices[3, 0, 1])
 
         # check the values of the pair 2
-        self.assertAlmostEqual(parameter["Port 3-Port 4"][0], self._matrices[0, 2, 3])
-        self.assertAlmostEqual(parameter["Port 3-Port 4"][1], self._matrices[1, 2, 3])
-        self.assertAlmostEqual(parameter["Port 3-Port 4"][2], self._matrices[2, 2, 3])
-        self.assertAlmostEqual(parameter["Port 3-Port 4"][3], self._matrices[3, 2, 3])
+        self.assertAlmostEqual(parameter[(2,3)][0], self._matrices[0, 2, 3])
+        self.assertAlmostEqual(parameter[(2,3)][1], self._matrices[1, 2, 3])
+        self.assertAlmostEqual(parameter[(2,3)][2], self._matrices[2, 2, 3])
+        self.assertAlmostEqual(parameter[(2,3)][3], self._matrices[3, 2, 3])
 
 if __name__ == '__main__':
     unittest.main()
