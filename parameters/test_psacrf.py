ed40e019cde072e78753e1bd8db3d20830e17a5b
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-06-15 11:24:33 -0400

    Fix PSACRF test

--- parameters/test_psacrf.py
+++ parameters/test_psacrf.py
@@ -6,17 +6,19 @@ from parameters.fext import FEXT
 from parameters.psacrf import PSACRF
 
 class TestPsAcrf(TestParameter):
-    def testComputeParameter(self):
+    def createParameter(self):
+        # we assume that psfext and il are tested
         fext = FEXT(self._ports, self._freq, self._matrices)
         psfext = PSFEXT(self._ports, self._freq, self._matrices, fext)
         il = InsertionLoss(self._ports, self._freq, self._matrices)
 
-        psacrf = PSACRF(self._ports, self._freq, self._matrices, psfext, il)
-        parameter = psacrf.getParameter()
-        #assume that psfext and il are tested
+        return PSACRF(self._ports, self._freq, self._matrices, psfext, il)
+
+    def testComputeParameter(self):
+        parameter = self._parameter.getParameter()
         
-        dbPsfext = psfext.getParameter()
-        dbIl = il.getParameter(full=True)
+        dbPSFEXT = self._parameter.getPSFEXT().getParameter()
+        dbIL = self._parameter.getIL().getParameter(full=True)
 
         # there should be a parameter for each ports
         self.assertEqual(len(parameter), len(self._ports))
@@ -28,28 +30,28 @@ class TestPsAcrf(TestParameter):
         self.assertEqual(len(parameter[3]), len(self._freq))
 
         # check the values of the port 1
-        self.assertAlmostEqual(parameter[0][0], dbPsfext[0][0]-dbIl[0][0])
-        self.assertAlmostEqual(parameter[0][1], dbPsfext[0][1]-dbIl[0][1])
-        self.assertAlmostEqual(parameter[0][2], dbPsfext[0][2]-dbIl[0][2])
-        self.assertAlmostEqual(parameter[0][3], dbPsfext[0][3]-dbIl[0][3])
+        self.assertAlmostEqual(parameter[0][0], dbPSFEXT[0][0]-dbIL[0][0])
+        self.assertAlmostEqual(parameter[0][1], dbPSFEXT[0][1]-dbIL[0][1])
+        self.assertAlmostEqual(parameter[0][2], dbPSFEXT[0][2]-dbIL[0][2])
+        self.assertAlmostEqual(parameter[0][3], dbPSFEXT[0][3]-dbIL[0][3])
 
         # check the values of the port 2
-        self.assertAlmostEqual(parameter[1][0], dbPsfext[1][0]-dbIl[1][0])
-        self.assertAlmostEqual(parameter[1][1], dbPsfext[1][1]-dbIl[1][1])
-        self.assertAlmostEqual(parameter[1][2], dbPsfext[1][2]-dbIl[1][2])
-        self.assertAlmostEqual(parameter[1][3], dbPsfext[1][3]-dbIl[1][3])
+        self.assertAlmostEqual(parameter[1][0], dbPSFEXT[1][0]-dbIL[1][0])
+        self.assertAlmostEqual(parameter[1][1], dbPSFEXT[1][1]-dbIL[1][1])
+        self.assertAlmostEqual(parameter[1][2], dbPSFEXT[1][2]-dbIL[1][2])
+        self.assertAlmostEqual(parameter[1][3], dbPSFEXT[1][3]-dbIL[1][3])
 
         # check the values of the port 3
-        self.assertAlmostEqual(parameter[2][0], dbPsfext[2][0]-dbIl[2][0])
-        self.assertAlmostEqual(parameter[2][1], dbPsfext[2][1]-dbIl[2][1])
-        self.assertAlmostEqual(parameter[2][2], dbPsfext[2][2]-dbIl[2][2])
-        self.assertAlmostEqual(parameter[2][3], dbPsfext[2][3]-dbIl[2][3])
+        self.assertAlmostEqual(parameter[2][0], dbPSFEXT[2][0]-dbIL[2][0])
+        self.assertAlmostEqual(parameter[2][1], dbPSFEXT[2][1]-dbIL[2][1])
+        self.assertAlmostEqual(parameter[2][2], dbPSFEXT[2][2]-dbIL[2][2])
+        self.assertAlmostEqual(parameter[2][3], dbPSFEXT[2][3]-dbIL[2][3])
 
         # check the values of the port 4
-        self.assertAlmostEqual(parameter[3][0], dbPsfext[3][0]-dbIl[3][0])
-        self.assertAlmostEqual(parameter[3][1], dbPsfext[3][1]-dbIl[3][1])
-        self.assertAlmostEqual(parameter[3][2], dbPsfext[3][2]-dbIl[3][2])
-        self.assertAlmostEqual(parameter[3][3], dbPsfext[3][3]-dbIl[3][3])
+        self.assertAlmostEqual(parameter[3][0], dbPSFEXT[3][0]-dbIL[3][0])
+        self.assertAlmostEqual(parameter[3][1], dbPSFEXT[3][1]-dbIL[3][1])
+        self.assertAlmostEqual(parameter[3][2], dbPSFEXT[3][2]-dbIL[3][2])
+        self.assertAlmostEqual(parameter[3][3], dbPSFEXT[3][3]-dbIL[3][3])
 
 if __name__ == '__main__':
     unittest.main()
