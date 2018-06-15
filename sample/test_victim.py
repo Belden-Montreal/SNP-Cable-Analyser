24f4c98a2f0c450b4d2cd12f1c456ac595b83535
Cedryk <cedryk.doucet@polymtl.ca>
2018-06-15 16:52:27 -0400

    Added Victim sample

---
+++ sample/test_victim.py
@@ -0,0 +1,24 @@
+import unittest
+from sample.test_sample import TestSample
+from sample.victim import Victim
+from parameters.axext import AXEXT
+from parameters.insertionloss import InsertionLoss
+from parameters.fext import FEXT
+
+class TestVictim(TestSample):
+    def setUp(self):
+        super(TestVictim, self).setUp()
+        self._params = ["IL", "AXEXTD", "PSAXEXT", "PSAACRX"]
+
+    def testParametersBuilding(self):
+        il = InsertionLoss(self._ports, self._freq, self._mm)
+        fext = FEXT(self._ports, self._freq, self._mm)
+        axextd = [AXEXT(self._ports, self._freq, self._mm, fext, il) for i in range(4)]
+        v = Victim(None, axextd)
+        self.setMockSample(v)
+        self.assertEqual(len(v._parameters), len(self._params))
+        self.assertListEqual(list(v._parameters.keys()), self._params)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ sample/victim.py
@@ -0,0 +1,26 @@
+from sample.sample import Sample, PORTS_NAME
+
+class Victim(Sample):
+
+    def __init__(self, snpFile, axextd):
+        self._axextd = axextd
+        super(Victim, self).__init__(snpFile)
+
+    def addParameters(self):
+        parameters = [
+            "IL",
+            "AXEXTD",
+            "PSAXEXT",
+            "PSAACRX",
+        ]
+
+        for parameter in parameters:
+            if parameter == "AXEXTD":
+                self._parameters[parameter] = self._axextd
+            else:
+                self._parameters[parameter] = self._factory.getParameter(parameter)
+
+    def setPorts(self):
+        for i in range(self._portsNumber//2):
+            self._ports[i] = PORTS_NAME[i]
+            self._ports[i+self._portsNumber//2] = "(d)"+PORTS_NAME[i]
