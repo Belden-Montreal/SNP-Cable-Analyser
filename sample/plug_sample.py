914bab46a6294c8d3999f326621f747826acaabb
Cedryk <cedryk.doucet@polymtl.ca>
2018-06-19 16:02:24 -0400

    Added plug sample

---
+++ sample/plug_sample.py
@@ -0,0 +1,30 @@
+from sample.sample import Sample
+
+class PlugSample(Sample):
+
+    def __init__(self, snpFile, openDelay, shortDelay, dfOpenDelay, dfShortDelay, k1, k2, k3):
+        self._openDelay = openDelay
+        self._shortDelay = shortDelay
+        self._dfOpenDelay = dfOpenDelay
+        self._dfShortDelay = dfShortDelay
+        self._k1 = k1
+        self._k2 = k2
+        self._k3 = k3
+        super(PlugSample, self).__init__(snpFile)
+
+    def addParameters(self):
+        parameters = [
+            "DFDelay",
+            "PlugDelay",
+            "NEXTDelay",
+            "CNEXT",
+        ]
+        self._parameters["DFShortDelay"] = self._dfShortDelay
+        self._parameters["DFOpenDelay"] = self._dfOpenDelay
+        self._parameters["PlugOpenDelay"] = self._openDelay
+        self._parameters["PlugShortDelay"] = self._shortDelay
+        self._parameters["k1"] = self._k1
+        self._parameters["k2"] = self._k2
+        self._parameters["k3"] = self._k3
+        for parameter in parameters:
+            self._parameters[parameter] = self._factory.getParameter(parameter)
