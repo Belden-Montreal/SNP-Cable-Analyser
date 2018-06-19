05cb29c1d6274ca9f4f6c1725d6d45ed4eb8d520
Cedryk <cedryk.doucet@polymtl.ca>
2018-06-19 16:03:44 -0400

    Added plug project for corrected next and delay calculations

---
+++ project/plug.py
@@ -0,0 +1,64 @@
+from project.project import Project
+from sample.delay import Delay
+from sample.plug_sample import PlugSample
+import numpy as np
+
+class Filetype():
+    OPEN = 0
+    SHORT = 1
+    LOAD = 2
+    DFOPEN = 3
+    DFSHORT = 4
+
+class Plug(Project):
+
+    def __init__(self):
+        super(Plug, self).__init__()
+        self._openDelay = None
+        self._shortDelay = None
+        self._loadSample = None
+        self._dfOpenDelay = None
+        self._dfShortDelay = None
+        self._k1 = 0
+        self._k2 = 0
+        self._k3 = 0
+
+    def importSamples(self, fileName, fileType=Filetype.OPEN):
+        if fileType == Filetype.OPEN:
+            openSample = Delay(fileName)
+            self._samples.append(openSample)
+            self._openDelay = openSample.getParameters()["Propagation Delay"]
+        elif fileType == Filetype.SHORT:
+            shortSample = Delay(fileName)
+            self._samples.append(shortSample)
+            self._shortDelay = shortSample.getParameters()["Propagation Delay"]
+        elif fileType == Filetype.LOAD:
+            self._loadSample = PlugSample(fileName, self._openDelay, self._shortDelay, self._dfOpenDelay, self._dfShortDelay, self._k1, self._k2, self._k3)
+            self._samples.append(self._loadSample)
+        elif fileType == Filetype.DFOPEN:
+            dfOpenSample = Delay(fileName)
+            self._samples.append(dfOpenSample)
+            self._dfOpenDelay = dfOpenSample.getParameters()["Propagation Delay"]
+        elif fileType == Filetype.DFSHORT:
+            dfShortSample = Delay(fileName)
+            self._samples.append(dfShortSample)
+            self._dfShortDelay = dfShortSample.getParameters()["Propagation Delay"]
+
+    def removeSamples(self, fileNames):
+        super(Plug, self).removeSamples(fileNames)
+
+    def getPlugNext(self):
+        return self._loadSample.getParameters()["CNEXT"]
+
+    def getNextDelay(self):
+        return self._loadSample.getParameters()["NEXTDelay"]
+
+    def setConstants(self, k1, k2, k3):
+        self._k1 = k1
+        self._k2 = k2
+        self._k3 = k3
+
+    def importFromXML(self):
+        raise NotImplementedError #TODO: read pre-calculated values from XML
+
+   
---
+++ project/test_plug.py
@@ -0,0 +1,33 @@
+import unittest
+from xlrd import open_workbook
+from project.plug import Plug, Filetype
+
+class TestPlug(unittest.TestCase):
+    def setUp(self):
+        self._fileName = "testout_mm.s8p"
+
+    def testFileImport(self):
+        project = Plug()
+
+        project.importSamples(self._fileName, Filetype.DFOPEN)
+        project.importSamples(self._fileName, Filetype.DFSHORT)
+        project.importSamples(self._fileName, Filetype.OPEN)
+        project.importSamples(self._fileName, Filetype.SHORT)
+        project.setConstants(1,2,3)
+        project.importSamples(self._fileName, Filetype.LOAD)
+
+        #should have added 5 samples
+        self.assertEqual(len(project._samples),5)
+
+        #samples should have the correct names
+        #self.assertEqual(project._samples[0]._name, "fci")
+        self.assertEqual(project._samples[0]._name, "testout_mm")
+
+        #should have access to plug NEXT and NEXT delay
+        self.assertTrue(project.getPlugNext())
+        self.assertTrue(project.getNextDelay())
+
+        project.removeSamples("testout_mm")
+        #should have removed the samples
+        self.assertEqual(len(project._samples), 0)
+
