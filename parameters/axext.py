b166243860e4caca2f50a3aceed76836890f62a0
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-07-11 14:21:35 -0400

    Use DataSerie in the AXEXT parameter

--- parameters/axext.py
+++ parameters/axext.py
@@ -1,6 +1,6 @@
-from parameters.parameter import PairedParameter, complex2db, order, diffDiffMatrix
+from parameters.parameter import Parameter, complex2db, order, diffDiffMatrix
 
-class AXEXT(PairedParameter):
+class AXEXT(Parameter):
     """
         AXEXT represents both the ANEXT and the AFEXT.
         It is calculated by taking both the FEXT and the Insertion Loss of the disturbed hardware
@@ -26,31 +26,27 @@ class AXEXT(PairedParameter):
 
     def __init__(self, ports, freq, matrices, fext, il):
         self._fext = fext
-        self._il = il
+        self._il   = il
         super(AXEXT, self).__init__(ports, freq, matrices)
 
-    def computePairs(self, ports):
-        # create each pair for the ANEXT
-        pairs = dict()
-        
-        pairs.update(self._fext.getPairs())
-        pairs.update(self._il.getPairs())
-
-        return pairs
+    def computeDataSeries(self):
+        return self._fext.getDataSeries().union(self._il.getDataSeries())
 
     def computeParameter(self):
         # initialize the dictionaries for each port
         (dbAXEXT, cpAXEXT) = (dict(), dict())
-        dbIl = self._il.getParameter(full=True)
-        cpIl = self._il.getComplexParameter(full=True)
-        dbFext = self._fext.getParameter()
-        cpFext = self._fext.getComplexParameter()
+
+        # get the parameters
+        dbIL   = self._il.getParameter()
+        cpIL   = self._il.getComplexParameter()
+        dbFEXT = self._fext.getParameter()
+        cpFEXT = self._fext.getComplexParameter()
         
         # extract the ANEXT values from the fext and il
-        dbAXEXT.update(dbFext)
-        cpAXEXT.update(cpFext)
-        dbAXEXT.update(dbIl)
-        cpAXEXT.update(cpIl)        
+        dbAXEXT.update(dbFEXT)
+        cpAXEXT.update(cpFEXT)
+        dbAXEXT.update(dbIL)
+        cpAXEXT.update(cpIL)        
         return (dbAXEXT, cpAXEXT)
 
     def chooseMatrices(self, matrices):
--- parameters/test_axext.py
+++ parameters/test_axext.py
@@ -5,29 +5,28 @@ from parameters.test_parameter import TestParameter
 from parameters.fext import FEXT
 from parameters.insertionloss import InsertionLoss
 from parameters.axext import AXEXT
+from parameters.dataserie import PortPairDataSerie
 
 class TestAXEXT(TestParameter):
     def createParameter(self):
         #we assume that fext and il are tested
-        fext = FEXT(self._e2ePorts, self._freq, self._matrices)
-        il = InsertionLoss(self._e2ePorts, self._freq, self._matrices)
-        return AXEXT(self._e2ePorts, self._freq, self._matrices, fext, il)
+        fext = FEXT(self._config, self._freq, self._matrices)
+        il   = InsertionLoss(self._config, self._freq, self._matrices)
+        return AXEXT(self._config, self._freq, self._matrices, fext, il)
 
-    def testComputePairs(self):
-        pairs = self._parameter.getPairs()
+    def testComputeDataSeries(self):
+        # for a 4 ports, there are 8 ANEXT data series (including reverse)
+        self.assertEqual(len(self._series), 8)
 
-        # for a 4 ports, there are 8 ANEXT pairs (including reverse)
-        self.assertEqual(len(pairs), 8)
-
-        # make sure the correct pairs were created
-        self.assertEqual((0,3) in pairs, True)
-        self.assertEqual((3,0) in pairs, True)
-        self.assertEqual((0,2) in pairs, True)
-        self.assertEqual((2,0) in pairs, True)
-        self.assertEqual((1,2) in pairs, True)
-        self.assertEqual((2,1) in pairs, True)
-        self.assertEqual((1,3) in pairs, True)
-        self.assertEqual((3,1) in pairs, True)     
+        # make sure the correct series were created
+        self.assertEqual(PortPairDataSerie(self._ports[0], self._ports[3]) in self._series, True)
+        self.assertEqual(PortPairDataSerie(self._ports[3], self._ports[0]) in self._series, True)
+        self.assertEqual(PortPairDataSerie(self._ports[0], self._ports[2]) in self._series, True)
+        self.assertEqual(PortPairDataSerie(self._ports[2], self._ports[0]) in self._series, True)
+        self.assertEqual(PortPairDataSerie(self._ports[1], self._ports[2]) in self._series, True)
+        self.assertEqual(PortPairDataSerie(self._ports[2], self._ports[1]) in self._series, True)
+        self.assertEqual(PortPairDataSerie(self._ports[1], self._ports[3]) in self._series, True)
+        self.assertEqual(PortPairDataSerie(self._ports[3], self._ports[1]) in self._series, True)         
 
     def testComputeParameter(self):
         parameter = self._parameter.getParameter()
@@ -36,46 +35,46 @@ class TestAXEXT(TestParameter):
         self.assertEqual(len(parameter), 8)
 
         # the number of sample should be the same as the number of frequencies
-        self.assertEqual(len(parameter[(0,3)]), len(self._freq))
-        self.assertEqual(len(parameter[(3,0)]), len(self._freq))
-        self.assertEqual(len(parameter[(1,2)]), len(self._freq))
-        self.assertEqual(len(parameter[(2,1)]), len(self._freq))
+        self.assertEqual(len(parameter[PortPairDataSerie(self._ports[0], self._ports[3])]), len(self._freq))
+        self.assertEqual(len(parameter[PortPairDataSerie(self._ports[3], self._ports[0])]), len(self._freq))
+        self.assertEqual(len(parameter[PortPairDataSerie(self._ports[1], self._ports[2])]), len(self._freq))
+        self.assertEqual(len(parameter[PortPairDataSerie(self._ports[2], self._ports[1])]), len(self._freq))
 
         # check the values of the pair (2,1)
-        self.assertEqual(parameter[(2,1)][0], (complex2db(self._matrices[0, 2, 1]), complex2phase(self._matrices[0, 2, 1])))
-        self.assertEqual(parameter[(2,1)][1], (complex2db(self._matrices[1, 2, 1]), complex2phase(self._matrices[1, 2, 1])))
-        self.assertEqual(parameter[(2,1)][2], (complex2db(self._matrices[2, 2, 1]), complex2phase(self._matrices[2, 2, 1])))
-        self.assertEqual(parameter[(2,1)][3], (complex2db(self._matrices[3, 2, 1]), complex2phase(self._matrices[3, 2, 1])))
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[2], self._ports[1])][0], self._matrices[0, 2, 1])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[2], self._ports[1])][1], self._matrices[1, 2, 1])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[2], self._ports[1])][2], self._matrices[2, 2, 1])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[2], self._ports[1])][3], self._matrices[3, 2, 1])
 
         # check the values of the pair (1,2)
-        self.assertEqual(parameter[(1,2)][0], (complex2db(self._matrices[0, 1, 2]), complex2phase(self._matrices[0, 1, 2])))
-        self.assertEqual(parameter[(1,2)][1], (complex2db(self._matrices[1, 1, 2]), complex2phase(self._matrices[1, 1, 2])))
-        self.assertEqual(parameter[(1,2)][2], (complex2db(self._matrices[2, 1, 2]), complex2phase(self._matrices[2, 1, 2])))
-        self.assertEqual(parameter[(1,2)][3], (complex2db(self._matrices[3, 1, 2]), complex2phase(self._matrices[3, 1, 2])))
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[2])][0], self._matrices[0, 1, 2])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[2])][1], self._matrices[1, 1, 2])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[2])][2], self._matrices[2, 1, 2])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[2])][3], self._matrices[3, 1, 2])
 
         # check the values of the pair (3,0)
-        self.assertEqual(parameter[(3,0)][0], (complex2db(self._matrices[0, 3, 0]), complex2phase(self._matrices[0, 3, 0])))
-        self.assertEqual(parameter[(3,0)][1], (complex2db(self._matrices[1, 3, 0]), complex2phase(self._matrices[1, 3, 0])))
-        self.assertEqual(parameter[(3,0)][2], (complex2db(self._matrices[2, 3, 0]), complex2phase(self._matrices[2, 3, 0])))
-        self.assertEqual(parameter[(3,0)][3], (complex2db(self._matrices[3, 3, 0]), complex2phase(self._matrices[3, 3, 0])))
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[3], self._ports[0])][0], self._matrices[0, 3, 0])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[3], self._ports[0])][1], self._matrices[1, 3, 0])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[3], self._ports[0])][2], self._matrices[2, 3, 0])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[3], self._ports[0])][3], self._matrices[3, 3, 0])
 
         # check the values of the pair (0,3)
-        self.assertEqual(parameter[(0,3)][0], (complex2db(self._matrices[0, 0, 3]), complex2phase(self._matrices[0, 0, 3])))
-        self.assertEqual(parameter[(0,3)][1], (complex2db(self._matrices[1, 0, 3]), complex2phase(self._matrices[1, 0, 3])))
-        self.assertEqual(parameter[(0,3)][2], (complex2db(self._matrices[2, 0, 3]), complex2phase(self._matrices[2, 0, 3])))
-        self.assertEqual(parameter[(0,3)][3], (complex2db(self._matrices[3, 0, 3]), complex2phase(self._matrices[3, 0, 3])))
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[3])][0], self._matrices[0, 0, 3])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[3])][1], self._matrices[1, 0, 3])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[3])][2], self._matrices[2, 0, 3])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[3])][3], self._matrices[3, 0, 3])
 
         # check the values of the pair (0,2)
-        self.assertEqual(parameter[(0,2)][0], (complex2db(self._matrices[0, 0, 2]), complex2phase(self._matrices[0, 0, 2])))
-        self.assertEqual(parameter[(0,2)][1], (complex2db(self._matrices[1, 0, 2]), complex2phase(self._matrices[1, 0, 2])))
-        self.assertEqual(parameter[(0,2)][2], (complex2db(self._matrices[2, 0, 2]), complex2phase(self._matrices[2, 0, 2])))
-        self.assertEqual(parameter[(0,2)][3], (complex2db(self._matrices[3, 0, 2]), complex2phase(self._matrices[3, 0, 2])))
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[2])][0], self._matrices[0, 0, 2])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[2])][1], self._matrices[1, 0, 2])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[2])][2], self._matrices[2, 0, 2])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[2])][3], self._matrices[3, 0, 2])
 
         # check the values of the pair (1,3)
-        self.assertEqual(parameter[(1,3)][0], (complex2db(self._matrices[0, 1, 3]), complex2phase(self._matrices[0, 1, 3])))
-        self.assertEqual(parameter[(1,3)][1], (complex2db(self._matrices[1, 1, 3]), complex2phase(self._matrices[1, 1, 3])))
-        self.assertEqual(parameter[(1,3)][2], (complex2db(self._matrices[2, 1, 3]), complex2phase(self._matrices[2, 1, 3])))
-        self.assertEqual(parameter[(1,3)][3], (complex2db(self._matrices[3, 1, 3]), complex2phase(self._matrices[3, 1, 3])))
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[3])][0], self._matrices[0, 1, 3])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[3])][1], self._matrices[1, 1, 3])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[3])][2], self._matrices[2, 1, 3])
+        self.assertComplexAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[3])][3], self._matrices[3, 1, 3])
 
     def testComputeComplexParameter(self):
         parameter = self._parameter.getComplexParameter()
@@ -84,45 +83,45 @@ class TestAXEXT(TestParameter):
         self.assertEqual(len(parameter), 8)
 
         # the number of sample should be the same as the number of frequencies
-        self.assertEqual(len(parameter[(0,3)]), len(self._freq))
-        self.assertEqual(len(parameter[(3,0)]), len(self._freq))
-        self.assertEqual(len(parameter[(1,2)]), len(self._freq))
-        self.assertEqual(len(parameter[(2,1)]), len(self._freq))
+        self.assertEqual(len(parameter[PortPairDataSerie(self._ports[0], self._ports[3])]), len(self._freq))
+        self.assertEqual(len(parameter[PortPairDataSerie(self._ports[3], self._ports[0])]), len(self._freq))
+        self.assertEqual(len(parameter[PortPairDataSerie(self._ports[1], self._ports[2])]), len(self._freq))
+        self.assertEqual(len(parameter[PortPairDataSerie(self._ports[2], self._ports[1])]), len(self._freq))
 
         # check the values of the pair (2,1)
-        self.assertEqual(parameter[(2,1)][0], self._matrices[0, 2, 1])
-        self.assertEqual(parameter[(2,1)][1], self._matrices[1, 2, 1])
-        self.assertEqual(parameter[(2,1)][2], self._matrices[2, 2, 1])
-        self.assertEqual(parameter[(2,1)][3], self._matrices[3, 2, 1])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[2], self._ports[1])][0], self._matrices[0, 2, 1])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[2], self._ports[1])][1], self._matrices[1, 2, 1])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[2], self._ports[1])][2], self._matrices[2, 2, 1])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[2], self._ports[1])][3], self._matrices[3, 2, 1])
 
         # check the values of the pair (1,2)
-        self.assertEqual(parameter[(1,2)][0], self._matrices[0, 1, 2])
-        self.assertEqual(parameter[(1,2)][1], self._matrices[1, 1, 2])
-        self.assertEqual(parameter[(1,2)][2], self._matrices[2, 1, 2])
-        self.assertEqual(parameter[(1,2)][3], self._matrices[3, 1, 2])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[2])][0], self._matrices[0, 1, 2])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[2])][1], self._matrices[1, 1, 2])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[2])][2], self._matrices[2, 1, 2])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[2])][3], self._matrices[3, 1, 2])
 
         # check the values of the pair (3,0)
-        self.assertEqual(parameter[(3,0)][0], self._matrices[0, 3, 0])
-        self.assertEqual(parameter[(3,0)][1], self._matrices[1, 3, 0])
-        self.assertEqual(parameter[(3,0)][2], self._matrices[2, 3, 0])
-        self.assertEqual(parameter[(3,0)][3], self._matrices[3, 3, 0])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[3], self._ports[0])][0], self._matrices[0, 3, 0])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[3], self._ports[0])][1], self._matrices[1, 3, 0])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[3], self._ports[0])][2], self._matrices[2, 3, 0])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[3], self._ports[0])][3], self._matrices[3, 3, 0])
 
         # check the values of the pair (0,3)
-        self.assertEqual(parameter[(0,3)][0], self._matrices[0, 0, 3])
-        self.assertEqual(parameter[(0,3)][1], self._matrices[1, 0, 3])
-        self.assertEqual(parameter[(0,3)][2], self._matrices[2, 0, 3])
-        self.assertEqual(parameter[(0,3)][3], self._matrices[3, 0, 3])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[3])][0], self._matrices[0, 0, 3])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[3])][1], self._matrices[1, 0, 3])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[3])][2], self._matrices[2, 0, 3])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[3])][3], self._matrices[3, 0, 3])
 
         # check the values of the pair (0,2)
-        self.assertEqual(parameter[(0,2)][0], self._matrices[0, 0, 2])
-        self.assertEqual(parameter[(0,2)][1], self._matrices[1, 0, 2])
-        self.assertEqual(parameter[(0,2)][2], self._matrices[2, 0, 2])
-        self.assertEqual(parameter[(0,2)][3], self._matrices[3, 0, 2])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[2])][0], self._matrices[0, 0, 2])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[2])][1], self._matrices[1, 0, 2])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[2])][2], self._matrices[2, 0, 2])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[0], self._ports[2])][3], self._matrices[3, 0, 2])
 
         # check the values of the pair (1,3)
-        self.assertEqual(parameter[(1,3)][0], self._matrices[0, 1, 3])
-        self.assertEqual(parameter[(1,3)][1], self._matrices[1, 1, 3])
-        self.assertEqual(parameter[(1,3)][2], self._matrices[2, 1, 3])
-        self.assertEqual(parameter[(1,3)][3], self._matrices[3, 1, 3])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[3])][0], self._matrices[0, 1, 3])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[3])][1], self._matrices[1, 1, 3])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[3])][2], self._matrices[2, 1, 3])
+        self.assertAlmostEqual(parameter[PortPairDataSerie(self._ports[1], self._ports[3])][3], self._matrices[3, 1, 3])  
 if __name__ == '__main__':
     unittest.main()
