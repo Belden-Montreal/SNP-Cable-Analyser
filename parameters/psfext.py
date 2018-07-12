87f4dca9166d71f1db67e17392fe0d15f185da69
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-07-12 09:32:21 -0400

    Use DataSerie in the PSFEXT parameter

--- parameters/psfext.py
+++ parameters/psfext.py
@@ -1,37 +1,70 @@
 from parameters.parameter import Parameter, diffDiffMatrix, complex2db, complex2phase
+from parameters.dataserie import PortDataSerie
 import numpy as np
+
+def powerSum(values):
+    return 10*np.log10(np.sum(list(map(lambda v: 10**(v[0]/10), values))))
 
 class PSFEXT(Parameter):
     '''
-        PSFEXT is calculated using the following formula:
+    PSFEXT is calculated using the following formula:
+
         PSFEXT_k = 10*log10(sum(FEXT_i,k))
         
-        where FEXT_i,k is the FEXT loss from wire i to wire k
+    where FEXT_i,k is the FEXT loss from wire i to wire k.
+
+    TODO: Subclass Parameter into PowerSumParameter since PSNEXT/PSFEXT are
+          pretty much the same.
     '''
     def __init__(self, ports, freq, matrices, fext):
         self._fext = fext
         super(PSFEXT, self).__init__(ports, freq, matrices)
 
+    def computeDataSeries(self):
+        mains   = self._ports.getMainPorts()
+        remotes = self._ports.getRemotePorts()
+
+        # create a serie for each port
+        series = set()
+        for p in self._ports.getPorts():
+            # find the pairs that affect our port
+            fextSeries = set()
+            for fextSerie in self._fext.getDataSeries():
+                (p1,p2) = fextSerie.getPorts()
+
+                if not p == p1:
+                    continue
+
+                fextSeries.add(fextSerie)
+
+            # create the serie for the port
+            series.add(PortDataSerie(p, data=fextSeries))
+
+        return series
+
     def computeParameter(self):
-        
-        psfext = dict()
-        cpPsfext = dict()
-        for port in self._ports:
-            psfext[port] = list()
-            cpPsfext[port] = list()
-        dbFext = self._fext.getParameter()
-        cpFext = self._fext.getComplexParameter()
-        ports = dbFext.keys()
+        # initialize the dictionary for each data serie
+        dbPSFEXT = {serie: list() for serie in self._series}
+        cpPSFEXT = {serie: list() for serie in self._series}
+
+        # compute the power sum FEXT
         for (f,_) in enumerate(self._freq):
-            for port in self._ports:
-                ps = 10.0*np.log10(np.sum([10**(dbFext[key][f][0]/10) for key in ports if (key[0] == port)]))
-                #psfext[port].append(ps)
-                cp = np.sum([cpFext[key][f] for key in ports if (key[0] == port)])
-                cpPsfext[port].append(cp)
-                #ps = complex2db(cp)
-                phase = complex2phase(cp)
-                psfext[port].append((ps, phase))
-        return psfext,cpPsfext
+            for serie in self._series:
+                fextSeries = serie.getData()
+
+                # get the FEXT values of these pairs
+                cpValues = [self._fext.getComplexParameter()[s][f] for s in fextSeries]
+                dbValues = [self._fext.getParameter()[s][f]        for s in fextSeries]
+
+                # compute PSFEXT
+                cpValue = np.sum(list(cpValues))
+                dbValue = (powerSum(dbValues), complex2phase(cpValue))
+
+                # add the value to the list
+                dbPSFEXT[serie].append(dbValue)
+                cpPSFEXT[serie].append(cpValue)
+
+        return (dbPSFEXT, cpPSFEXT)
 
     def chooseMatrices(self, matrices):
         return diffDiffMatrix(matrices)
--- parameters/test_psfext.py
+++ parameters/test_psfext.py
@@ -1,61 +1,91 @@
 import unittest
-import numpy as np
+
 from parameters.test_parameter import TestParameter
-from parameters.psfext import PSFEXT
 from parameters.fext import FEXT
-from parameters.parameter import complex2db
+from parameters.psfext import PSFEXT
+from parameters.dataserie import PortDataSerie, PortPairDataSerie
+from sample.port import NetworkPort, PlugConfiguration
+import numpy as np
 
-def powersum(fext, f, port):
-    keys = fext.keys()
-    return 10*np.log10(np.sum([10**(fext[key][f][0]/10) for key in keys if (key[0] == port ) ]))
+def powerSum(values):
+    return 10*np.log10(np.sum(list(map(lambda v: 10**(v[0]/10), values))))
 
-class TestPsFEXT(TestParameter):
+class TestPSFEXT(TestParameter):
+    def setUp(self):
+        super(TestPSFEXT, self).setUp()
+
+        self._dataseries = {
+            0: PortDataSerie(self._ports[0]),
+            1: PortDataSerie(self._ports[1]),
+            2: PortDataSerie(self._ports[2]),
+            3: PortDataSerie(self._ports[3]),
+        }
+
+        self._fextseries = {
+            0: PortPairDataSerie(self._ports[0], self._ports[3]),
+            1: PortPairDataSerie(self._ports[1], self._ports[2]),
+            2: PortPairDataSerie(self._ports[2], self._ports[1]),
+            3: PortPairDataSerie(self._ports[3], self._ports[0]),
+        }
+
     def createParameter(self):
-        # we assume that FEXT is tested
-        fext = FEXT(self._e2ePorts, self._freq, self._matrices)
+        fext = FEXT(self._config, self._freq, self._matrices)
+        return PSFEXT(self._config, self._freq, self._matrices, fext)
 
-        return PSFEXT(self._e2ePorts, self._freq, self._matrices, fext)
+    def testComputeDataSeries(self):
+        self.assertEqual(len(self._series), 4)
+        self.assertEqual(self._dataseries[0] in self._series, True)
+        self.assertEqual(self._dataseries[1] in self._series, True)
+        self.assertEqual(self._dataseries[2] in self._series, True)
+        self.assertEqual(self._dataseries[3] in self._series, True)
+        for serie in self._series:
+            fextSeries = serie.getData()
+            self.assertEqual(len(fextSeries), 1)
+            if serie == self._dataseries[0]:
+                self.assertEqual(self._fextseries[0] in fextSeries, True)
+            if serie == self._dataseries[1]:
+                self.assertEqual(self._fextseries[1] in fextSeries, True)
+            if serie == self._dataseries[2]:
+                self.assertEqual(self._fextseries[2] in fextSeries, True)
+            if serie == self._dataseries[3]:
+                self.assertEqual(self._fextseries[3] in fextSeries, True)
 
     def testComputeParameter(self):
         parameter = self._parameter.getParameter()
-
-        dbFEXT = self._parameter.getFEXT().getParameter()
+        fext = self._parameter.getFEXT().getParameter()
 
         # there should be a parameter for each ports
         self.assertEqual(len(parameter), len(self._ports))
 
         # the number of sample should be the same as the number of frequencies
-        self.assertEqual(len(parameter[0]), len(self._freq))
-        self.assertEqual(len(parameter[1]), len(self._freq))
-        self.assertEqual(len(parameter[2]), len(self._freq))
-        self.assertEqual(len(parameter[3]), len(self._freq))
+        self.assertEqual(len(parameter[self._dataseries[0]]), len(self._freq))
+        self.assertEqual(len(parameter[self._dataseries[1]]), len(self._freq))
+        self.assertEqual(len(parameter[self._dataseries[2]]), len(self._freq))
+        self.assertEqual(len(parameter[self._dataseries[3]]), len(self._freq))
+
+        # check the values of the port 0
+        self.assertAlmostEqual(parameter[self._dataseries[0]][0][0], powerSum([fext[self._fextseries[0]][0]]))
+        self.assertAlmostEqual(parameter[self._dataseries[0]][1][0], powerSum([fext[self._fextseries[0]][1]]))
+        self.assertAlmostEqual(parameter[self._dataseries[0]][2][0], powerSum([fext[self._fextseries[0]][2]]))
+        self.assertAlmostEqual(parameter[self._dataseries[0]][3][0], powerSum([fext[self._fextseries[0]][3]]))
 
         # check the values of the port 1
-        self.assertAlmostEqual(parameter[0][0][0], powersum(dbFEXT, 0, 0))
-        self.assertAlmostEqual(parameter[0][1][0], powersum(dbFEXT, 1, 0))
-        self.assertAlmostEqual(parameter[0][2][0], powersum(dbFEXT, 2, 0))
-        self.assertAlmostEqual(parameter[0][3][0], powersum(dbFEXT, 3, 0))
+        self.assertAlmostEqual(parameter[self._dataseries[1]][0][0], powerSum([fext[self._fextseries[1]][0]]))
+        self.assertAlmostEqual(parameter[self._dataseries[1]][1][0], powerSum([fext[self._fextseries[1]][1]]))
+        self.assertAlmostEqual(parameter[self._dataseries[1]][2][0], powerSum([fext[self._fextseries[1]][2]]))
+        self.assertAlmostEqual(parameter[self._dataseries[1]][3][0], powerSum([fext[self._fextseries[1]][3]]))
 
         # check the values of the port 2
-        self.assertAlmostEqual(parameter[1][0][0], powersum(dbFEXT, 0, 1))
-        self.assertAlmostEqual(parameter[1][1][0], powersum(dbFEXT, 1, 1))
-        self.assertAlmostEqual(parameter[1][2][0], powersum(dbFEXT, 2, 1))
-        self.assertAlmostEqual(parameter[1][3][0], powersum(dbFEXT, 3, 1))
+        self.assertAlmostEqual(parameter[self._dataseries[2]][0][0], powerSum([fext[self._fextseries[2]][0]]))
+        self.assertAlmostEqual(parameter[self._dataseries[2]][1][0], powerSum([fext[self._fextseries[2]][1]]))
+        self.assertAlmostEqual(parameter[self._dataseries[2]][2][0], powerSum([fext[self._fextseries[2]][2]]))
+        self.assertAlmostEqual(parameter[self._dataseries[2]][3][0], powerSum([fext[self._fextseries[2]][3]]))
 
         # check the values of the port 3
-        self.assertAlmostEqual(parameter[2][0][0], powersum(dbFEXT, 0, 2))
-        self.assertAlmostEqual(parameter[2][1][0], powersum(dbFEXT, 1, 2))
-        self.assertAlmostEqual(parameter[2][2][0], powersum(dbFEXT, 2, 2))
-        self.assertAlmostEqual(parameter[2][3][0], powersum(dbFEXT, 3, 2))
-
-        # check the values of the port 4
-        self.assertAlmostEqual(parameter[3][0][0], powersum(dbFEXT, 0, 3))
-        self.assertAlmostEqual(parameter[3][1][0], powersum(dbFEXT, 1, 3))
-        self.assertAlmostEqual(parameter[3][2][0], powersum(dbFEXT, 2, 3))
-        self.assertAlmostEqual(parameter[3][3][0], powersum(dbFEXT, 3, 3))
-
-        # check complex parameter
-        self.assertAlmostEqual(parameter[0][0][0], complex2db(self._parameter.getComplexParameter()[0][0]))
+        self.assertAlmostEqual(parameter[self._dataseries[3]][0][0], powerSum([fext[self._fextseries[3]][0]]))
+        self.assertAlmostEqual(parameter[self._dataseries[3]][1][0], powerSum([fext[self._fextseries[3]][1]]))
+        self.assertAlmostEqual(parameter[self._dataseries[3]][2][0], powerSum([fext[self._fextseries[3]][2]]))
+        self.assertAlmostEqual(parameter[self._dataseries[3]][3][0], powerSum([fext[self._fextseries[3]][3]]))
 
 if __name__ == '__main__':
     unittest.main()
