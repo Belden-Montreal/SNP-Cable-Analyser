b94e0690105f6c532ecad6903a131cc9fdae7525
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-07-12 16:12:01 -0400

    Add possibility to remove remote pairs from NEXT

--- parameters/next.py
+++ parameters/next.py
@@ -18,23 +18,30 @@ class NEXT(Parameter):
 
     We have the following pairs twice: (1,2), (1,3), (2,3), (4,5), (4,6), (5,6).
     """
+    def __init__(self, ports, freq, matrices, mains=True, remotes=True):
+        self._mains   = mains
+        self._remotes = remotes
+        super(NEXT, self).__init__(ports, freq, matrices)
+
     def computeDataSeries(self):
         # create each pair for the NEXT
         series = set()
 
         # get the NEXT pairs between the main ports
-        mains = self._ports.getMainPorts()
-        for (i,j) in itertools.product(mains, mains):
-            if i is j:
-                continue
-            series.add(PortPairDataSerie(i, j))
+        if self._mains:
+            mains = self._ports.getMainPorts()
+            for (i,j) in itertools.product(mains, mains):
+                if i is j:
+                    continue
+                series.add(PortPairDataSerie(i, j))
  
         # get the NEXT pairs between the remote ports
-        remotes = self._ports.getRemotePorts()
-        for (i,j) in itertools.product(remotes, remotes):
-            if i is j:
-                continue
-            series.add(PortPairDataSerie(i, j))
+        if self._remotes:
+            remotes = self._ports.getRemotePorts()
+            for (i,j) in itertools.product(remotes, remotes):
+                if i is j:
+                    continue
+                series.add(PortPairDataSerie(i, j))
 
         return series
 
