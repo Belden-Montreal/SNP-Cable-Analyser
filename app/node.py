62f069813102a3288241df7bcf927d73a4726d59
Cedryk <cedryk.doucet@polymtl.ca>
2018-07-12 14:09:24 -0400

    Changed model and item to QStandardItem

--- app/node.py
+++ app/node.py
@@ -1,44 +1,18 @@
+from PyQt5 import QtGui, QtCore
 
-class Node(object):
-    def __init__(self, name, parent=None):
-        self.parent = parent
-        if parent:
-            parent.addChild(self)
-        self._name = name
-        self.children = []
+class Node(QtGui.QStandardItem):
+    def __init__(self, name):
+        super(Node, self).__init__(name)
         self._dataObject = None
+        self.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
 
-    def addChild(self, item):
-        self.children.append(item)
-        item.parent = self
-
-    def removeChild(self, item):
-        self.children.remove(item)
+    def removeRow(self, row):
+        super(Node, self).removeRow(row)
         self.delete()
-
-    def child(self, row):
-        return self.children[row]
-
-    def childCount(self):
-        return len(self.children)
-
-    def row(self):
-        if self.parent:
-            return self.parent.children.index(self)
-        return 0
-    
-    def columnCount(self):
-        return 1
-
-    def data(self, column):
-        return self._name
-
-    def getName(self):
-        return self._name
 
     def getObject(self):
         return self._dataObject
 
     def delete(self):
-        if self.childCount() == 0:
-            self.parent.removeChild(self)
+        if self.rowCount() == 0:
+            self.parent().removeRow(self.row())
