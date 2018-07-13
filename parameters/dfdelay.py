073987a5f84f4d037219e8d68978beb133b1867e
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-07-13 11:16:02 -0400

    Use DataSerie in the DF delay parameter

--- parameters/dfdelay.py
+++ parameters/dfdelay.py
@@ -1,32 +1,30 @@
-from parameters.parameter import PairedParameter, takeClosest
+from parameters.parameter import Parameter, takeClosest
+from parameters.dataserie import PortDataSerie
 import numpy as np
 
-class DFDelay(PairedParameter):
-
+class DFDelay(Parameter):
     def __init__(self, ports, freq, matrices, openDelay, shortDelay):
-        self._openDelay = openDelay
+        self._openDelay  = openDelay
         self._shortDelay = shortDelay
+        self._visible    = False
         super(DFDelay, self).__init__(ports, freq, matrices)
-        self._visible = False
 
-    def computePairs(self, ports):
-        pairs = dict()
-        for i in range(len(ports)):
-            port,isRemote = ports[i]
-            pairs[(i,i)] = (port,isRemote)
-        return pairs
+    def computeDataSeries(self):
+        return {PortDataSerie(port) for port in self._ports.getPorts()}
 
     def computeParameter(self):
-        openDelay = self._openDelay.getParameter()
+        openDelay  = self._openDelay.getParameter()
         shortDelay = self._shortDelay.getParameter()
+
         dfDelay = dict()
-        for port in self._ports:
-            #find the indexes of the points closest to 100 and 500 MHz
-            f100, f500 = self.__getFrequenciesIndex(100, 500, self._freq)
-            openAvg = np.mean(openDelay[port][f100:f500])
-            shortAvg = np.mean(shortDelay[port][f100:f500])
-            dfDelay[port] = (openAvg + shortAvg)/4.0
-        return dfDelay,None
+        for serie in self._series:
+            # find the indexes of the points closest to 100 and 500 MHz
+            (f100, f500) = self.__getFrequenciesIndex(100, 500, self._freq)
+
+            openAvg  = np.mean(openDelay[serie][f100:f500])
+            shortAvg = np.mean(shortDelay[serie][f100:f500])
+            dfDelay[serie] = (openAvg + shortAvg)/4.0
+        return (dfDelay, None)
 
     def chooseMatrices(self, matrices):
         return None
@@ -41,4 +39,4 @@ class DFDelay(PairedParameter):
         return "DFDelay"
 
     def __getFrequenciesIndex(self, f1, f2, f):
-        return takeClosest(f1, f), takeClosest(f2, f)+1
+        return (takeClosest(f1, f), takeClosest(f2, f)+1)
