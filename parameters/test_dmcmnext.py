b3f5823671f31168de7ccb4c07b9d1a6f79dfbda
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-07-13 10:27:46 -0400

    Use DataSerie in the DMCMNEXT parameter

--- parameters/test_dmcmnext.py
+++ parameters/test_dmcmnext.py
@@ -1,71 +1,84 @@
 import unittest
 
-from parameters.parameter import complex2db, complex2phase
 from parameters.test_parameter import TestParameter
 from parameters.dmcmnext import DMCMNEXT
+from parameters.dataserie import PortPairDataSerie
 
 class TestDMCMNEXT(TestParameter):
     def createParameter(self):
-        return DMCMNEXT(self._e2ePorts, self._freq, self._matrices)
+        self._dataseries = {
+            0: PortPairDataSerie(self._ports[0], self._ports[1]),
+            1: PortPairDataSerie(self._ports[1], self._ports[0]),
+            2: PortPairDataSerie(self._ports[2], self._ports[3]),
+            3: PortPairDataSerie(self._ports[3], self._ports[2]),
+        }
 
-    def testComputePairs(self):
-        seDmcmnext = DMCMNEXT(self._ports, self._freq, self._matrices)
-        pairs = seDmcmnext.getPairs()
+        return DMCMNEXT(self._config, self._freq, self._matrices)
 
-        # for a 4 ports single ended, there are 6 DMCMNEXT pairs
-        self.assertEqual(len(pairs), 6)
-
-        # make sure the correct pairs are there
-        self.assertEqual((0,1) in pairs, True)
-        self.assertEqual((2,3) in pairs, True)
-
-        pairs = self._parameter.getParameter().keys()
-        #for a 4 ports end-to-end, there are 4 NEXT pairs
-        self.assertEqual(len(pairs), 2)
+    def testComputeDataSeries(self):
+        self.assertEqual(self._series, set(self._dataseries.values()))
 
     def testComputeParameter(self):
         parameter = self._parameter.getParameter()
 
-        # for a 4 ports, there is only 2 DMCMNEXT pairs
-        self.assertEqual(len(parameter), 2)
-
         # the number of sample should be the same as the number of frequencies
-        self.assertEqual(len(parameter[(0,1)]), len(self._freq))
-        self.assertEqual(len(parameter[(2,3)]), len(self._freq))
+        for serie in self._dataseries.values():
+            self.assertEqual(len(parameter[serie]), len(self._freq))
 
         # check the values of the pair (0,1)
-        self.assertAlmostEqual(parameter[(0,1)][0], (complex2db(self._matrices[0, 0, 5]), complex2phase(self._matrices[0, 0, 5])))
-        self.assertAlmostEqual(parameter[(0,1)][1], (complex2db(self._matrices[1, 0, 5]), complex2phase(self._matrices[1, 0, 5])))
-        self.assertAlmostEqual(parameter[(0,1)][2], (complex2db(self._matrices[2, 0, 5]), complex2phase(self._matrices[2, 0, 5])))
-        self.assertAlmostEqual(parameter[(0,1)][3], (complex2db(self._matrices[3, 0, 5]), complex2phase(self._matrices[3, 0, 5])))
+        self.assertComplexAlmostEqual(parameter[self._dataseries[0]][0], self._matrices[0, 0, 5])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[0]][1], self._matrices[1, 0, 5])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[0]][2], self._matrices[2, 0, 5])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[0]][3], self._matrices[3, 0, 5])
+
+        # check the values of the pair (1,0)
+        self.assertComplexAlmostEqual(parameter[self._dataseries[1]][0], self._matrices[0, 1, 4])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[1]][1], self._matrices[1, 1, 4])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[1]][2], self._matrices[2, 1, 4])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[1]][3], self._matrices[3, 1, 4])
 
         # check the values of the pair (2,3)
-        self.assertAlmostEqual(parameter[(2,3)][0], (complex2db(self._matrices[0, 2, 7]), complex2phase(self._matrices[0, 2, 7])))
-        self.assertAlmostEqual(parameter[(2,3)][1], (complex2db(self._matrices[1, 2, 7]), complex2phase(self._matrices[1, 2, 7])))
-        self.assertAlmostEqual(parameter[(2,3)][2], (complex2db(self._matrices[2, 2, 7]), complex2phase(self._matrices[2, 2, 7])))
-        self.assertAlmostEqual(parameter[(2,3)][3], (complex2db(self._matrices[3, 2, 7]), complex2phase(self._matrices[3, 2, 7])))
+        self.assertComplexAlmostEqual(parameter[self._dataseries[2]][0], self._matrices[0, 2, 7])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[2]][1], self._matrices[1, 2, 7])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[2]][2], self._matrices[2, 2, 7])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[2]][3], self._matrices[3, 2, 7])
+
+        # check the values of the pair (3,2)
+        self.assertComplexAlmostEqual(parameter[self._dataseries[3]][0], self._matrices[0, 3, 6])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[3]][1], self._matrices[1, 3, 6])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[3]][2], self._matrices[2, 3, 6])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[3]][3], self._matrices [3, 3, 6])
 
     def testComputeComplexParameter(self):
         parameter = self._parameter.getComplexParameter()
 
-        # for a 4 ports, there is only 2 DMCMNEXT pairs (including reverse)
-        self.assertEqual(len(parameter), 2)
-
         # the number of sample should be the same as the number of frequencies
-        self.assertEqual(len(parameter[(0,1)]), len(self._freq))
-        self.assertEqual(len(parameter[(2,3)]), len(self._freq))
+        for serie in self._dataseries.values():
+            self.assertEqual(len(parameter[serie]), len(self._freq))
 
         # check the values of the pair (0,1)
-        self.assertAlmostEqual(parameter[(0,1)][0], self._matrices[0, 0, 5])
-        self.assertAlmostEqual(parameter[(0,1)][1], self._matrices[1, 0, 5])
-        self.assertAlmostEqual(parameter[(0,1)][2], self._matrices[2, 0, 5])
-        self.assertAlmostEqual(parameter[(0,1)][3], self._matrices[3, 0, 5])
+        self.assertAlmostEqual(parameter[self._dataseries[0]][0], self._matrices[0, 0, 5])
+        self.assertAlmostEqual(parameter[self._dataseries[0]][1], self._matrices[1, 0, 5])
+        self.assertAlmostEqual(parameter[self._dataseries[0]][2], self._matrices[2, 0, 5])
+        self.assertAlmostEqual(parameter[self._dataseries[0]][3], self._matrices[3, 0, 5])
+
+        # check the values of the pair (1,0)
+        self.assertAlmostEqual(parameter[self._dataseries[1]][0], self._matrices[0, 1, 4])
+        self.assertAlmostEqual(parameter[self._dataseries[1]][1], self._matrices[1, 1, 4])
+        self.assertAlmostEqual(parameter[self._dataseries[1]][2], self._matrices[2, 1, 4])
+        self.assertAlmostEqual(parameter[self._dataseries[1]][3], self._matrices[3, 1, 4])
 
         # check the values of the pair (2,3)
-        self.assertAlmostEqual(parameter[(2,3)][0], self._matrices[0, 2, 7])
-        self.assertAlmostEqual(parameter[(2,3)][1], self._matrices[1, 2, 7])
-        self.assertAlmostEqual(parameter[(2,3)][2], self._matrices[2, 2, 7])
-        self.assertAlmostEqual(parameter[(2,3)][3], self._matrices[3, 2, 7])
+        self.assertAlmostEqual(parameter[self._dataseries[2]][0], self._matrices[0, 2, 7])
+        self.assertAlmostEqual(parameter[self._dataseries[2]][1], self._matrices[1, 2, 7])
+        self.assertAlmostEqual(parameter[self._dataseries[2]][2], self._matrices[2, 2, 7])
+        self.assertAlmostEqual(parameter[self._dataseries[2]][3], self._matrices[3, 2, 7])
+
+        # check the values of the pair (3,2)
+        self.assertAlmostEqual(parameter[self._dataseries[3]][0], self._matrices[0, 3, 6])
+        self.assertAlmostEqual(parameter[self._dataseries[3]][1], self._matrices[1, 3, 6])
+        self.assertAlmostEqual(parameter[self._dataseries[3]][2], self._matrices[2, 3, 6])
+        self.assertAlmostEqual(parameter[self._dataseries[3]][3], self._matrices[3, 3, 6])
 
 if __name__ == '__main__':
     unittest.main()
