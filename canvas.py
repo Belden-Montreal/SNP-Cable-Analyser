ece1692783dc4f5888b72a5c1497a2b6539dcbab
Cedryk <cedryk.doucet@polymtl.ca>
2018-07-13 16:32:30 -0400

    Added a figure in canvas constructor

--- canvas.py
+++ canvas.py
@@ -1,8 +1,9 @@
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas 
-import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas 
 
 class Canvas(FigureCanvas):
-    def __init__(self, parent=None):
-        self.figure = plt.figure()
-        FigureCanvas.__init__(self, self.figure)
+    def __init__(self, figure, parent=None):
+        FigureCanvas.__init__(self, figure)
         self.setParent(parent)
+
+    def changeFigure(self, figure):
+        FigureCanvas.__init__(self, figure)
