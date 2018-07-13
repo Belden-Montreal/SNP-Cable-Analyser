0dfe9bd8ea04f58caa0482b58ecb1a2b6b7d9108
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-07-13 09:58:08 -0400

    Add test for normal parameter in TCLT

--- parameters/test_tctl.py
+++ parameters/test_tctl.py
@@ -17,6 +17,25 @@ class TestTCTL(TestParameter):
         self.assertEqual(self._series, set(self._dataseries.values()))
 
     def testComputeParameter(self):
+        parameter = self._parameter.getParameter()
+
+        # the number of sample should be the same as the number of frequencies
+        for serie in self._dataseries.values():
+            self.assertEqual(len(parameter[serie]), len(self._freq))
+
+        # check the values of the port 1
+        self.assertComplexAlmostEqual(parameter[self._dataseries[0]][0], self._matrices[0, 4, 2])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[0]][1], self._matrices[1, 4, 2])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[0]][2], self._matrices[2, 4, 2])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[0]][3], self._matrices[3, 4, 2])
+
+        # check the values of the port 2
+        self.assertComplexAlmostEqual(parameter[self._dataseries[1]][0], self._matrices[0, 5, 3])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[1]][1], self._matrices[1, 5, 3])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[1]][2], self._matrices[2, 5, 3])
+        self.assertComplexAlmostEqual(parameter[self._dataseries[1]][3], self._matrices[3, 5, 3])
+
+    def testComputeComplexParameter(self):
         parameter = self._parameter.getComplexParameter()
 
         # the number of sample should be the same as the number of frequencies
