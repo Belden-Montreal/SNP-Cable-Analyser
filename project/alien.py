e82319d8e6cf31ec60dd22627a133bfaee7ac464
Cedryk <cedryk.doucet@polymtl.ca>
2018-07-16 10:30:07 -0400

    Moved import functions

--- project/alien.py
+++ project/alien.py
@@ -23,6 +23,27 @@ class Alien(Project):
             self._victims[end]["PSANEXT"] = None
             self._victims[end]["PSAACRF"] = None 
 
+    def importSamples(self, fileNames, end, param, disturber=False):
+        if disturber:
+            pool = ThreadPool()
+            samples = pool.map(self.__createDisturber, fileNames)
+            if self._victims[end][param] is not None:
+                self._victims[end][param].calculateAXEXTD(samples)
+            self._disturbers[end][param] = samples
+            return samples
+        elif len(fileNames) < 2:
+            sample = self.__createVictim(fileNames[0])
+            if len(self._disturbers[end][param]) > 0:
+                sample.calculateAXEXTD(self._disturbers[end][param])
+            self._victims[end][param] = sample
+            return sample
+
+    def __createDisturber(self, name):
+        return Disturber(name)
+
+    def __createVictim(self, name):
+        return Victim(name)
+
     def removeSample(self, sample):
         for end in self._disturbers:
             for param in self._disturbers[end]:
@@ -30,6 +51,12 @@ class Alien(Project):
                     self._disturbers[end][param].remove(sample)
                 elif sample == self._victims[end][param]:
                     self._victims[end][param] = None
+
+    def disturbers(self):
+        return self._disturbers
+
+    def victims(self):
+        return self._victims
 
     def generateExcel(self, outputName, sampleNames, z=False):
         raise NotImplementedError
@@ -45,42 +72,20 @@ class AlienNode(ProjectNode):
 
     def openImportWindow(self, parent):
         dial = AlienImportDialog(parent)
-        disturbersFile, victimFile, end, param = dial.getFiles()
-        samples = list()
-        disturbers = self.importSamples(disturbersFile, disturber=True)
-        victim = self.importSamples([victimFile], disturber=False)
-        samples.extend(disturbers)
-        samples.append(victim)
-        self._dataObject._disturbers[end][param] = disturbers
-        self._dataObject._victims[end][param] = victim
-        self.addChildren(samples, end, param)
-
-    def importSamples(self, fileNames, disturber=False):
-        if disturber:
-            pool = ThreadPool()
-            samples = pool.map(self.__createDisturber, fileNames)
-            self._axextd = self.calculateAXEXTD(samples)
-            return samples
-        elif len(fileNames) < 2:
-                return self.__createVictim(fileNames[0], self._axextd)
-                
-
-    def __createDisturber(self, name):
-        return Disturber(name)
-
-    def __createVictim(self, name, axextd):
-        return Victim(name, axextd)
-
-    def calculateAXEXTD(self, disturbers):
-        return [disturber.getParameters()["AXEXT"] for disturber in disturbers]
-
+        files = dial.getFiles()
+        if files:
+            disturbersFile, victimFile, end, param = dial.getFiles()
+            samples = list()
+            samples = self._dataObject.importSamples(disturbersFile, end, param, disturber=True)
+            samples.append(self._dataObject.importSamples([victimFile], end, param, disturber=False))
+            self.addChildren(samples, end, param)
 
     def addChildren(self, samples, end, param):
         node = self.hasChild(end)
         if not node:
             node = Node(end)
             self.appendRow(node)
-        subNode = self.hasChild(param)
+        subNode = node.hasChild(param)
         if not subNode:
             subNode = Node(param)
             node.appendRow(subNode)
@@ -89,11 +94,11 @@ class AlienNode(ProjectNode):
             subNode.appendRow(SampleNode(sample, self._dataObject))
 
     def setupInitialData(self):
-        for end, params in self._dataObject._disturbers.items():
+        for end, params in self._dataObject.disturbers().items():
             for param, samples in params.items():
                 if len(samples):
                     self.addChildren(samples, end, param)
 
     def getWidgets(self):
-        alienTab = AlienWidget(self._dataObject)
+        alienTab = AlienWidget(self)
         return {"Alien": alienTab}
