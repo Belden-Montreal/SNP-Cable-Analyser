d2a8e23be6efba1bcc7b48bcb668bf3c9b5bd94f
Cedryk <cedryk.doucet@polymtl.ca>
2018-07-16 10:29:57 -0400

    Added import functions to the widget buttons

--- widgets/alien_widget.py
+++ widgets/alien_widget.py
@@ -7,10 +7,11 @@ from canvas import Canvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 class AlienWidget(TabWidget, alien_widget_ui.Ui_Form):
-    def __init__(self, alien):
+    def __init__(self, alienNode):
         super(AlienWidget, self).__init__(self)
         self._figure = Figure()
-        self._alien = alien
+        self._alien = alienNode.getObject()
+        self._node = alienNode
         self.graphicsView = Canvas(self._figure)
         self.verticalLayout.insertWidget(0, self.graphicsView)
         self.navBar = NavigationToolbar(self.graphicsView, self)
@@ -23,13 +24,15 @@ class AlienWidget(TabWidget, alien_widget_ui.Ui_Form):
         self.endGroup.addButton(self.alienEnd1)
         self.endGroup.addButton(self.alienEnd2)
         self.endGroup.buttonClicked.connect(lambda: self.updateWidget())
+        self.alienVictimButton.clicked.connect(lambda: self.importVictim())
+        self.alienImportSNP.clicked.connect(lambda: self.importDisturbers())
         self.drawFigure("End 1", "PSANEXT")
 
     def updateWidget(self):
         test = self.testTypeGroup.checkedButton().text()
         end = self.endGroup.checkedButton().text()
         self.alienDisturbers.clear()
-        for disturber in self._alien._disturbers[end][test]:
+        for disturber in self._alien.disturbers()[end][test]:
             item = QtWidgets.QListWidgetItem()
             item.setText(disturber.getName())
             self.alienDisturbers.addItem(item)
@@ -37,7 +40,7 @@ class AlienWidget(TabWidget, alien_widget_ui.Ui_Form):
 
     def drawFigure(self, end, test):
         self._figure.clear()
-        sample = self._alien._victims[end][test]
+        sample = self._alien.victims()[end][test]
         if test == "PSANEXT":
             names = ["PSANEXT", "PSAACRN"]
         else:
@@ -62,6 +65,22 @@ class AlienWidget(TabWidget, alien_widget_ui.Ui_Form):
                 ax.grid(which='both')
                 ax.legend(loc='best')
         self.showTab()
+
+    def importVictim(self):
+        fileName,_ = QtWidgets.QFileDialog.getOpenFileName(self, "Select victim", "", "sNp Files (*.s*p)")
+        test = self.testTypeGroup.checkedButton().text()
+        end = self.endGroup.checkedButton().text()
+        sample = self._alien.importSamples([fileName], end, test, disturber=False)
+        self._node.addChildren([sample], end, test)
+        self.updateWidget()
+
+    def importDisturbers(self):
+        files,_ = QtWidgets.QFileDialog.getOpenFileNames(self, "Select disturbers", "", "sNp Files (*.s*p)")
+        test = self.testTypeGroup.checkedButton().text()
+        end = self.endGroup.checkedButton().text()
+        samples = self._alien.importSamples(files, end, test, disturber=True)
+        self._node.addChildren(samples, end, test)
+        self.updateWidget()
     
     def showTab(self):
         self.graphicsView.draw()
