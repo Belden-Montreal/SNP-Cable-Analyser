bbf966247879b0cd90f2402751bf355fc124ec19
Cedryk <cedryk.doucet@polymtl.ca>
2018-07-18 15:09:59 -0400

    Updated the plug project to use the new widget

--- project/plug.py
+++ project/plug.py
@@ -17,6 +17,31 @@ class Plug(Project):
         self._k2 = 0
         self._k3 = 0
 
+    def importDfOpen(self, fileName):
+        self._dfOpenDelay = Delay(fileName)
+        return self._dfOpenDelay
+
+    def importDfShort(self, fileName):
+        self._dfShortDelay = Delay(fileName)
+        return self._dfShortDelay
+
+    def importOpen(self, fileName):
+        self._openDelay = Delay(fileName)
+        return self._openDelay
+
+    def importShort(self, fileName):
+        self._shortDelay = Delay(fileName)
+        return self._shortDelay
+
+    def importLoad(self, fileName):
+        self._loadSample = PlugSample(fileName, 
+            self._openDelay.getParameters()["Propagation Delay"],
+            self._shortDelay.getParameters()["Propagation Delay"],
+            self._dfOpenDelay.getParameters()["Propagation Delay"],
+            self._dfShortDelay.getParameters()["Propagation Delay"],
+            self._k1, self._k2, self._k3)
+        return self._loadSample
+
     def getPlugNext(self):
         return self._loadSample.getParameters()["CNEXT"]
 
@@ -39,33 +64,41 @@ class Plug(Project):
 
 
 from sample.sample import SampleNode
+from widgets.plug_widget import PlugWidget
+
 class PlugNode(ProjectNode):
+    def __init__(self, plug):
+        super(PlugNode, self).__init__(plug)
+        self._plugWidget = None
 
     def openImportWindow(self, parent):
         dial = PlugImportDialog(parent)
         files = dial.getFiles()
         if files:
             dfOpen, dfShort, plugOpen, plugShort, plugLoad, k1, k2, k3 = files
-            openSample = Delay(plugOpen)
-            shortSample = Delay(plugShort)
-            dfOpenSample = Delay(dfOpen)
-            dfShortSample = Delay(dfShort)
-            loadSample = PlugSample(plugLoad, 
-                                    openSample.getParameters()["Propagation Delay"],
-                                    shortSample.getParameters()["Propagation Delay"],
-                                    dfOpenSample.getParameters()["Propagation Delay"],
-                                    dfShortSample.getParameters()["Propagation Delay"],
-                                    k1, k2, k3)
+            openSample = self._dataObject.importOpen(plugOpen)
+            shortSample = self._dataObject.importShort(plugShort)
+            dfOpenSample = self._dataObject.importDfOpen(dfOpen)
+            dfShortSample = self._dataObject.importDfShort(dfShort)
+            self._dataObject.setConstants(k1, k2, k3)
+            loadSample = self._dataObject.importLoad(plugLoad)
 
             samples = [openSample, shortSample, dfOpenSample, dfShortSample, loadSample]
             self.addChildren(samples)
-            self._dataObject.setConstants(k1, k2, k3)
             self._dataObject._samples = samples
-            self._dataObject._loadSample = loadSample
+            if self._plugWidget:
+                self._plugWidget.createTabs()
+                self._plugWidget.updateWidget()
 
     def addChildren(self, samples):
         for sample in samples:
             self.appendRow(SampleNode(sample, self._dataObject))
 
     def setupInitialData(self):
-        self.addChildren(self._dataObject._samples)
+        samples = [self._dataObject._openDelay, self._dataObject._shortDelay, self._dataObject._dfOpenDelay, self._dataObject._dfShortDelay, self._dataObject._loadSample]
+        self.addChildren(samples)
+
+    def getWidgets(self):
+        if not self._plugWidget:
+            self._plugWidget = PlugWidget(self)
+        return {"Plug": self._plugWidget}
