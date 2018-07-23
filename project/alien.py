a7d14b00d983d4d6a7a9eff02a0a745e4db683a6
Cedryk <cedryk.doucet@polymtl.ca>
2018-07-23 15:48:31 -0400

    Added alien excel export

--- project/alien.py
+++ project/alien.py
@@ -4,6 +4,7 @@ from sample.victim import Victim
 from project.alien_import_dialog import AlienImportDialog
 from multiprocessing.dummy import Pool as ThreadPool
 from copy import deepcopy
+import xlsxwriter
 
 class Alien(Project):
     '''
@@ -71,7 +72,53 @@ class Alien(Project):
         return self._victims
 
     def generateExcel(self, outputName, sampleNames, z=False):
-        raise NotImplementedError
+        workbook = xlsxwriter.Workbook(outputName, options={'nan_inf_to_errors': True})
+
+        for name, ends in self._victims.items():
+            worksheet = workbook.add_worksheet(name)
+            worksheet.write('A1', 'Alien ID:')
+            worksheet.write('B1', self._name)
+
+            cell_format = workbook.add_format({'align': 'center',
+                                                'valign': 'vcenter',
+                                                'border': 6,})
+            worksheet.merge_range('A3:A5', "Frequency", cell_format)
+
+            curPos = 1
+            for end, sample in ends.items():
+                if sample:
+                    worksheet.merge_range(1, curPos, 1, curPos+sample.getNumPorts()*2-1, end, cell_format)
+                    for i, (paramName, parameter) in enumerate(sample.getParameters().items()):
+                        numSignals = 0
+                        try:
+                            if parameter.getName() == "PSAXEXT" or parameter.getName() == "PSAACRX":
+                                numSignals = len(parameter.getPorts())
+                                worksheet.merge_range(2, curPos, 2, curPos+numSignals*2-1,  paramName, cell_format)
+                                for i, (key, (portName,_)) in enumerate(parameter.getPorts().items()):
+                                    worksheet.merge_range(3, curPos+i*2, 3, curPos+i*2+1, str(portName), cell_format)
+                                    # TODO: alien phase/complex values
+                                    # if z:
+                                    #     worksheet.write(4,curPos+i*2, "real", cell_format)
+                                    #     worksheet.write(4,curPos+i*2+1, "imaginary", cell_format)
+                                    #     param = parameter.getComplexParameter()
+                                    #     print(param)
+                                    #     for j,data in enumerate(param[key]):
+                                    #         worksheet.write(5+j, 0, sample.getFrequencies()[j])
+                                    #         self.box(workbook, worksheet, param, key, i*2, j, data.real, curPos)
+                                    #         self.box(workbook, worksheet, param, key, i*2+1, j, data.imag, curPos)
+                                    # else:
+                                    worksheet.write(4,curPos+i*2, "mag", cell_format)
+                                    worksheet.write(4,curPos+i*2+1, "phase", cell_format)
+                                    param = parameter.getParameter()
+                                    for j, (mag, phase) in enumerate(param[key]):
+                                        worksheet.write(5+j, 0, sample.getFrequencies()[j])
+                                        self.box(workbook, worksheet, param, key, i*2, j, mag, curPos)
+                                        self.box(workbook, worksheet, param, key, i*2+1, j, phase, curPos)
+                        except AttributeError as e:
+                            print(e)
+                    
+                        curPos += numSignals*2
+        workbook.close()
 
     def nodeFromProject(self):
         return AlienNode(self)
