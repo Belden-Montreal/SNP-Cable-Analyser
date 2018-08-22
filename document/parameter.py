94145baea17e097b4f6658287e10d7e67f1fd8c0
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-08-22 15:39:11 -0400

    Use parameter configuration its document object

--- document/parameter.py
+++ document/parameter.py
@@ -1,28 +1,36 @@
 from document.object import DocumentObject, normalize
+from analysis.parameter import ParameterAnalysis
 from overrides import overrides
 
 class ParameterDocumentObject(DocumentObject):
-    def __init__(self, prefix, analysis):
-        self._analysis = analysis
-        super(ParameterDocumentObject, self).__init__(prefix)
-
     @overrides
     def getBasename(self):
-        return normalize(self._analysis.getParameter().getName())
+        return normalize(self.getConfiguration().getParameter().getName())
 
     @overrides
     def getTemplateName(self):
         return "parameter.tex"
 
     @overrides
-    def getTemplateArguments(self):
-        figure = str(self.getPrefix().joinpath(self.getBasename()+".pgf"))
-        self._analysis.getFigure().savefig(figure)
+    def getTemplateArguments(self, configuration):
+        arguments = dict()
 
-        return {
-            'figure': {
-                'path': figure,
-                'scale': 0.3,
-            },
-        }
+        # create the analysis with the right data series
+        parameter = configuration.getParameter()
+        series    = {s for (s,e) in configuration.getDataSeries().items() if e}
+        analysis  = ParameterAnalysis(parameter, series=series)
+        analysis.setFormat(configuration.getFormat())
+        analysis.setScale(configuration.getScale())
+
+        # save the figure
+        relative = str(self.getPrefix().joinpath(self.getBasename()+".pgf"))
+        figure = str(self.getRoot().joinpath(relative))
+        analysis.getFigure().savefig(figure)
+
+        # create the arguments
+        arguments["figure"] = dict()
+        arguments["figure"]["path"]  = relative
+        arguments["figure"]["scale"] = 0.45
+
+        return arguments
         
