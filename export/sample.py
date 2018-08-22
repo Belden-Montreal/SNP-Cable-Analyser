09a5077941681f5170f7060bb66b309060cb9295
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-08-22 09:54:36 -0400

    Call super constructor in SampleExportConfiguration

--- export/sample.py
+++ export/sample.py
@@ -3,13 +3,15 @@ from export.parameter import ParameterExportConfiguration
 from overrides import overrides
 
 class SampleExportConfiguration(ExportConfiguration):
-    def __init__(self, sample):
+    def __init__(self, sample, **kwargs):
+        super(SampleExportConfiguration, self).__init__(**kwargs)
         self._sample     = sample
         self._parameters = dict()
 
         # by default, we export all parameters
         for (ptype, parameter) in sample.getParameters().items():
             self._parameters[ptype] = ParameterExportConfiguration(parameter)
+            self._parameters[ptype].setExport(True)
 
     def addParameter(self, parameter):
         if parameter in self._parameters:
