9914e629c5912fe81189f59bf3946f75160ce3f9
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-08-22 15:39:37 -0400

    Use sample configuration its document object

--- document/sample.py
+++ document/sample.py
@@ -2,34 +2,33 @@ from document.object import DocumentObject, normalize, latex
 from overrides import overrides
 
 class SampleDocumentObject(DocumentObject):
-    def __init__(self, prefix, sample):
-        self._sample = sample
-        super(SampleDocumentObject, self).__init__(prefix)
-
     @overrides
     def getBasename(self):
-        return normalize(self._sample.getName())
+        return normalize(self.getConfiguration().getSample().getName())
 
     @overrides
     def getTemplateName(self):
         return "sample.tex"
 
     @overrides
-    def getTemplateArguments(self):
+    def getTemplateArguments(self, configuration):
         arguments = dict()
+        arguments["name"] = latex(configuration.getSample().getName())
+        arguments["parameters"] = list()
 
-        # create the parameter document objects
-        arguments['name'] = latex(self._sample.getName())
-        arguments['parameters'] = set()
-        for (ptype, analysis) in self._sample.getAnalyses().items():
-            # make sure the analysis is set to export
-            if not analysis.doExport():
+        for (ptype, config) in configuration.getParameters().items():
+            if not config.doExport():
                 continue
 
-            # generate the analysis' document object
-            name = normalize(analysis.getParameter().getName())
-            path = self.getPrefix().joinpath(name)
-            obj = analysis.generateDocumentObject(path)
-            arguments['parameters'].add(obj.getFilePath())
+            # create the document object for the parameter
+            name   = normalize(config.getParameter().getName())
+            root   = self.getRoot()
+            prefix = self.getPrefix().joinpath(name)
+            docobj = config.generateDocumentObject(root, prefix)
+
+            # create the arguments for this sample
+            parameter = dict()
+            parameter["path"] = docobj.getRelativePath()
+            arguments["parameters"].append(parameter)
 
         return arguments
