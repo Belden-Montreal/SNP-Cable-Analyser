528f343751789f52757763d4457c5721b08d3991
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-08-23 11:43:12 -0400

    Use setuptools for packaging the application

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
-from cx_Freeze import setup, Executable
-import os
-os.environ['TCL_LIBRARY'] = r'C:\Users\LXF09011\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6'
-os.environ['TK_LIBRARY'] = r'C:\Users\LXF09011\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6'
-additional_mods = ['numpy.core._methods', 'numpy.lib.format', 'matplotlib', "tkinter", "scipy.spatial.ckdtree", "visa", "pyvisa"]
+from setuptools import setup, find_packages
 
+setup(
+    name="snpanalyzer",
+    version="0.1",
 
-setup(name = "Belden SNP Analyzer 2" ,
-      version = "0.1" ,
-      description = "" ,
-      
-      options = {'build_exe': {"packages":['pkg_resources._vendor', "skrf", "tkinter","numpy", "scipy", "matplotlib", "visa", "pyvisa"],
-                               'includes': additional_mods,
-                               'include_files': ['favicon.ico', 'C:/Users/LXF09011/AppData/Local/Programs/Python/Python36-32/DLLs/tcl86t.dll', 'C:/Users/LXF09011/AppData/Local/Programs/Python/Python36-32/DLLs/tk86t.dll']}},
-      executables = [Executable("GuiMain.py",targetName="Belden SNP Analyzer 2.exe",  icon="favicon.ico")])
+    packages=find_packages(),
+
+    description="This package analyses SNP files.",
+
+    license="GPLv3",
+
+    package_data={
+        "snpanalyzer.template" : ["*.tex"],
+    },
+
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "overrides",
+        "pathlib",
+        "pyqt5",
+        "pyvisa",
+        "scikit-rf",
+        "scipy",
+        "sympy",
+        "xlsxwriter",
+        "xmltodict",
+    ],
+
+    url="https://github.com/liamaltarac/SNP-Cable-Analyser",
+)
+
 
